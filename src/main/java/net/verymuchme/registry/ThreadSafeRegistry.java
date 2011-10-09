b829f0e79fb613b4388b89132ca0cf5e7e209a1d
Tracy Flynn <projects@olioinfo.net>
2011-10-09 05:31:53 -0400

    Added method to retrieve an object with a supplied default object.

--- src/main/java/net/verymuchme/registry/ThreadSafeRegistry.java
+++ src/main/java/net/verymuchme/registry/ThreadSafeRegistry.java
@@ -2,6 +2,8 @@ package net.verymuchme.registry;
 
 import java.util.HashMap;
 
+import net.verymuchme.commonstatus.CommonStatus;
+
 /**
  * <p>Provides a thread-safe location to store objects that need to be shared across threads.</p>
  * 
@@ -25,6 +27,29 @@ public class ThreadSafeRegistry {
 	public static Object retrieve(String registryName) {
 		synchronized(ThreadSafeRegistry.class) {
 			return ThreadSafeRegistry.registry.get(registryName);
+		}
+	}
+
+	/**
+	 * Get a registered object. Register the default if registered object does not exist. Thread-safe.
+	 * 
+	 * @param registryName
+	 * @param defaultObject
+	 * @return CommonStatus object - status = true if registered object found, false otherwise. Object returned in property specified by supplied registryName.
+	 */
+	public static CommonStatus retrieve(String registryName, Object defaultObject) {
+		synchronized(ThreadSafeRegistry.class) {
+			boolean registeredObjectFound = true;
+			Object registeredObject = ThreadSafeRegistry.registry.get(registryName);
+			if (registeredObject == null) {
+				registeredObject = defaultObject;
+				ThreadSafeRegistry.register(registryName, registeredObject);
+				registeredObjectFound = false;
+			}
+			CommonStatus status = new CommonStatus();
+			status.setStatus(registeredObjectFound);
+			status.setOjectProperty(registryName, registeredObject);
+			return status;
 		}
 	}
 	
